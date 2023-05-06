f75ac01488c08c7cacbe9ef9d0cbe7a635916437
Rodion <rodionserez@gmail.com>
2023-05-06 14:28:01 +0300

    feat: add solution of the second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const rl = require('readline');
+
+const cmd = rl.createInterface(process.stdin, process.stdout);
+const fileWriteStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+console.log('Hi, dude! Enter text!');
+
+cmd
+	.on('line', inputData => inputData === 'exit' ? cmd.close() : fileWriteStream.write(inputData))
+	.on('close', () => console.log('Bye bye!'));
