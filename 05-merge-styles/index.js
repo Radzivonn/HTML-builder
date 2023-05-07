2ae4b9dcae61e55cb75bf76f01449b193f240046
Rodion <rodionserez@gmail.com>
2023-05-07 14:58:18 +0300

    refactor: break code into functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,21 +1,31 @@
-const fs = require('fs');
+const { readdir } =  require('node:fs/promises');
+const { createReadStream, createWriteStream, stat } =  require('node:fs');
 const path = require('path');
 
-const bundleWriteStream = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
-
-fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
-  if (err) throw err;
-  else {
-    files.forEach(file => {
-      const cssFilePath = path.join(__dirname, 'styles/', file);
-      fs.stat(cssFilePath,
-        (err, stats) => {
-          if (err) throw err;
-          if (stats.isFile(file) && path.extname(file) === '.css') {
-            const cssReadStream = fs.createReadStream(cssFilePath, 'utf-8');
-						cssReadStream.pipe(bundleWriteStream);
-          }
-        });
-    });
+async function getFiles (dirPath) {
+  try {
+    return await readdir(dirPath);
+  } catch (err) {
+    console.error(err);
   }
-});
+}
+
+const writeToBundle = (fileName, filePath, writeStream) => {
+  stat(filePath,
+    (err, stats) => {
+      if (err) throw err;
+      if (stats.isFile(fileName) && path.extname(fileName) === '.css') {
+        const cssReadStream = createReadStream(filePath, 'utf-8');
+        cssReadStream.pipe(writeStream);
+      }
+    });
+};
+
+const createBundle = (files) => {
+  const bundleWriteStream = createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
+  files.forEach(file => {
+    writeToBundle(file, path.join(__dirname, 'styles/', file), bundleWriteStream);
+  });
+};
+
+getFiles(path.join(__dirname, 'styles')).then(data => createBundle(data));
