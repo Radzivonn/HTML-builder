0ad508307632ea70d136d29520a969ec39af0a11
Rodion <rodionserez@gmail.com>
2023-05-07 14:51:15 +0300

    refactor: replace tabs with indents

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -7,6 +7,6 @@ const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8'
 let data = '';
 
 readStream
-	.on('data', chunk => data += chunk)
-	.on('end', () => stdout.write(data))
-	.on('error', error => stderr.write(error.message));
+  .on('data', chunk => data += chunk)
+  .on('end', () => stdout.write(data))
+  .on('error', error => stderr.write(error.message));
