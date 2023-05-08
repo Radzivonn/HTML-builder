f3296f6ca8af25c966fba5e2f2bbf1ea1e9f1ead
Rodion <rodionserez@gmail.com>
2023-05-08 20:27:10 +0300

    feat: implement copy assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,8 @@
-const { readdir, readFile } =  require('node:fs/promises');
-const { mkdir, createReadStream, createWriteStream, stat } =  require('node:fs');
+const { readdir, readFile, open, rm } =  require('node:fs/promises');
+const { mkdir, createReadStream, createWriteStream, stat, copyFile } =  require('node:fs');
 const path = require('path');
 
+/* Functions */
 const createFolder = (folderPath) => {
   mkdir(folderPath, { recursive: true }, err => {
     if (err) throw err;
@@ -51,12 +52,48 @@ async function createBundleCSS() {
   });
 }
 
+async function copyFileToDist(srcFilePath, copyFilePath) {
+  let filehandle; 
+  try {
+    filehandle = await open(copyFilePath, 'w');
+    copyFile(srcFilePath, copyFilePath, err => {
+      if (err) throw err;
+    });
+  } finally {
+    await filehandle.close();
+  }
+}
+
+const copyFiles = (files, srcPath, distPath) => {
+  files.forEach(file => {
+    const srcFilePath = path.join(srcPath, file);
+    const copyFilePath = path.join(distPath, file);
+    stat(srcFilePath,
+      (err, stats) => {
+        if (err) throw err;
+        if (!stats.isFile(file)) copyDir(file, srcFilePath, distPath); /* recursively copy dir */
+        else {
+          copyFileToDist(srcFilePath, copyFilePath);
+        }
+      });
+  });
+};
+
+const copyDir = (folderName, srcPath, distPath) => {
+  const copyFilesPath = path.join(distPath, folderName);
+  createFolder(copyFilesPath);
+  getFiles(srcPath).then(files => copyFiles(files, srcPath, copyFilesPath));
+};
+/* Functions */
+
+/* build */
 
 createFolder(path.join(__dirname, 'project-dist')); // create dist folder
 
 const templateFileReadStream = createReadStream(path.join(__dirname, 'template.html'));
-let pageLayout = '';
+let pageLayout = ''; // string that contains HTML layout
 
+/* build a page from components */
 templateFileReadStream
   .on('data', data => pageLayout += data)
   .on('end', () => {
@@ -64,3 +101,9 @@ templateFileReadStream
   });
 
 createBundleCSS();
+
+/* delete project-dist/assets(if it exists) and copy it again */
+rm(path.join(__dirname, 'project-dist/assets'), { recursive: true, force: true })
+  .then(() => copyDir('assets', path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist')));
+
+/* build */
